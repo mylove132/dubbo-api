c5028770a73a3e5adcffd6dc24f3f4dc28bfb148
刘占会 <liuzhanhui@okay.cn>
2019-07-12 15:27:56 +0800

    修改redis配置

---
+++ src/main/java/com/dubbo/api/config/DataJedisConfiguration.java
@@ -0,0 +1,53 @@
+package com.dubbo.api.config;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.SpringBootConfiguration;
+import org.springframework.context.annotation.Bean;
+import org.springframework.data.redis.connection.jedis.JedisConnectionFactory;
+import redis.clients.jedis.JedisPool;
+import redis.clients.jedis.JedisPoolConfig;
+
+/**
+ * @Author: liuzhanhui
+ * @Decription:
+ * @Date: Created in 2019-07-12:15:15
+ * Modify date: 2019-07-12:15:15
+ */
+@Slf4j
+@SpringBootConfiguration
+public class DataJedisConfiguration {
+    @Value("${redis.host}")
+    private  String host;
+    @Value("${redis.password}")
+    private  String password;
+    @Value("${redis.port}")
+    private  int port;
+    @Value("${redis.timeout}")
+    private  int timeout;
+    @Value("${redis.jedis.pool.max-idle}")
+    private int maxIdle;
+    @Value("${redis.jedis.pool.max-wait}")
+    private long maxWaitMillis;
+
+    @Bean
+    JedisConnectionFactory jedisConnectionFactory() {
+        log.info("Create JedisConnectionFactory successful");
+        JedisConnectionFactory factory = new JedisConnectionFactory();
+        factory.setHostName(host);
+        factory.setPort(port);
+        factory.setTimeout(timeout);
+        factory.setPassword(password);
+        return factory;
+    }
+    @Bean
+    public JedisPool redisPoolFactory() {
+        log.info("JedisPool init successful，host -> [{}]；port -> [{}]", host, port);
+        JedisPoolConfig jedisPoolConfig = new JedisPoolConfig();
+        jedisPoolConfig.setMaxIdle(maxIdle);
+        jedisPoolConfig.setMaxWaitMillis(maxWaitMillis);
+
+        JedisPool jedisPool = new JedisPool(jedisPoolConfig, host, port, timeout, password);
+        return jedisPool;
+    }
+}
